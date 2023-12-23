9dad8c21bdd9dc8eae6daa54f088916c2ac6ce0c
Narotam-Mishra <narotam21jan@gmail.com>
2023-12-23 16:29:33 +0530

    product-store-app_1.3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
   return (
     <div className="p-4">
-      <div className="flex justify-center items-center gap-x-4">
+      <div className="flex justify-center items-center gap-x-4" style={{width:"20%", marginLeft:"37%"}}>
         <button
           className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
           onClick={() => setShowType("table")}
@@ -43,9 +43,9 @@ const Home = () => {
       </div>
       <div className="flex justify-between items-center">
         {showType === "table" ? (
-          <h1 className="text-3xl my-8 mx-auto">Product Table View</h1>
+          <h1 className="text-3xl my-8 mx-auto">Products Table View</h1>
         ) : (
-          <h1 className="text-3xl my-8 mx-auto">Product Card View</h1>
+          <h1 className="text-3xl my-8 mx-auto">Products Card View</h1>
         )}
         <Link to={"/product/add"}>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
